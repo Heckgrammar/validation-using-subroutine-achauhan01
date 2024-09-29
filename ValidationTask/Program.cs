91e4c2a40576a4191e0a14522b0fcc4c674833fa
achauhan01 <159480070+achauhan01@users.noreply.github.com>
2024-09-29 21:17:05 +0100

    Update Program.cs

--- ValidationTask/Program.cs
+++ ValidationTask/Program.cs
@@ -1,8 +1,8 @@
-﻿namespace ValidationTask
+namespace ValidationTask
 {
     internal class Program
     {
-        static void Main(string[] args)
+       static void Main(string[] args)
         {
             string firstName, lastName, username, password, emailAddress;
             int age;
@@ -11,17 +11,22 @@
             // The username should only be output once
             Console.Write("Enter first name: ");
             firstName = Console.ReadLine();
+            ValidName(firstName);
             Console.Write("Enter last name: ");
             lastName = Console.ReadLine();
+            ValidName(lastName);
             Console.Write("Enter age: ");
             age = Convert.ToInt32(Console.ReadLine());
+            validAge(age);
             Console.Write("Enter Password: ");
             password = Console.ReadLine();
+            ValidPassword(password);
             Console.Write("Enter email address: ");
             emailAddress = Console.ReadLine();
+            validEmail(emailAddress);
 
 
-            username = createUserName(firstName,lastName,age);
+            username = createUserName(firstName, lastName, age);
             Console.WriteLine($"Username is {username}, you have successfully registered please remember your password");
 
             //  Test your program with a range of tests to show all validation works
@@ -41,53 +46,40 @@
 
         static bool validAge(int age)
         {
-                //age must be between 11 and 18 inclusive
-                if (age >= 11 && age <= 18)
-                {
-                    return true;
-                }
-                return false;
+            //age must be between 11 and 18 inclusive
+            if (age >= 11 && age <= 18)
+            {
+                return true;
+            }
+            return false;
         }
 
-   
+
         static bool ValidPassword(string password)
         {
             // Check password is at least 8 characters in length
-
+            int charNum = password.Length; //gets the length of password
+            if (charNum >= 8)
+            {
+                return true;
+            }
 
             // Check password contains a mix of lower case, upper case and non letter characters
-            int charNum = password.Length; //gets the length of password
-                if (charNum >= 8)
-                {
-                    return true;
-                }
-                return false;
-            byte[] ASCII = Encoding.ASCII.GetBytes(password);
-            int[] checkLowerCase = Enumerable.Range(65, 90).ToArray();
-            int[] checkUpperCase = Enumerable.Range(97, 122).ToArray();
-            int[] checkNumber = Enumerable.Range(48, 57).ToArray();
-            int[] checkSpecialChar1 = Enumerable.Range(32, 47).ToArray();
-            int[] checkSpecialChar2 = Enumerable.Range(58, 64).ToArray();
-            int[] checkSpecialChar3 = Enumerable.Range(91, 96).ToArray();
-            int[] checkSpecialChar4 = Enumerable.Range(123, 126).ToArray();
-            if (ASCII.Contains(checkLowerCase) && (ASCII.Contains(checkUpperCase)))
-            {
-
-            }
             // QWErty%^& = valid
             // QWERTYUi = not valid
             // ab£$%^&* = not valid
             // QWERTYu! = valid
+            string pattern = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$!£|^*+=%~#@_-])$";
+            if (Regex.IsMatch(password, pattern))
+            {
+                return true;
+            }
 
-            //if (password.Contains(65,90) && password.Contains(97,122))
-            
             // Check password contains no runs of more than 2 consecutive or repeating letters or numbers
             // AAbbdd!2 = valid (only 2 consecutive letters A and B and only 2 repeating of each)
             // abC461*+ = not valid (abC are 3 consecutive letters)
             // 987poiq! = not valid (987 are consecutive)
-
-
-
+            return false;
         }
         static bool validEmail(string email)
         {
@@ -97,6 +89,7 @@
             // has at least 2 characters after the .
             // contains only one @ and any number of .
             // does not contain any other non letter or number characters
+            return false;
 
         }
         static string createUserName(string firstName, string lastName, int age)
